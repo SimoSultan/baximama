910b532ba0672c2b2b8a23bb1e1de0cc4ef1ca1a
Simon Curran <simon.curran@entaingroup.com.au>
2023-09-27 22:36:53 +1000

    feat(site): initialises vite, react, tailwind, i18n

---
+++ src/components/order/Order.jsx
@@ -0,0 +1,18 @@
+import { Card } from "../core";
+
+export default function Order() {
+  return (
+    <Card>
+      <h1>Order</h1>
+      <p>
+        Tempor exercitation voluptate minim enim laborum. Et officia fugiat
+        veniam nulla excepteur excepteur sint voluptate voluptate deserunt ipsum
+        non. Elit ipsum sunt dolor non fugiat elit elit aliquip aute do magna
+        sit do enim. Ut laborum esse enim pariatur duis exercitation esse
+        labore. Deserunt incididunt dolore incididunt aute sint culpa est
+        excepteur nostrud. Ea consequat nulla ipsum consectetur. Magna
+        consectetur labore deserunt nostrud non officia enim culpa minim.
+      </p>
+    </Card>
+  );
+}
